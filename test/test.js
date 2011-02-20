2b6fd2591a011bdb0b32f9a2a67eb50c6b7836b9
Gabriel Farrell <gsf747@gmail.com>
2011-02-20 09:20:49 -0500

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,37 @@
+var fs = require('fs');
+var selleck = require('./selleck');
+
+var template = fs.readFileSync('blog.html', 'utf8');
+
+var context = {
+  blog: {
+    title: "Scrumptious lessons",
+  },
+  posts: [
+    {
+      title: 'A happy time',
+      content: 'In my childhood I would laugh and play.',
+      author: 'Hornsby Sumpin'
+    },
+    {
+      title: 'Better days',
+      content: 'Pals are a necessity in this lonely world.',
+      author: 'Baffle McCough',
+      comments: [
+        {
+          content: 'I stand in complete agreement.',
+          author: 'Bitters Compote'
+        },
+        {
+          content: 'What a baleful load of beeswax.',
+          author: 'Gluglug Baldag'
+        }
+      ]
+    }
+  ]
+};
+
+var partials = {};
+
+var rendered = selleck.render(template, context, partials);
+console.log(rendered);
