b10c462afe19e91b76c39bd0f57e5b9b18beeb64
Gabriel Farrell <gsf747@gmail.com>
2011-03-05 18:33:14 -0500

    Updated test-blog, included compile

--- test/test-blog.js
+++ test/test-blog.js
@@ -1,9 +1,11 @@
+// test basic blog template
+
+var fs = require('fs');
 var common = require('./common');
 var assert = common.assert;
-var fs = require('fs');
 var selleck = require('../lib/selleck');
 
-common.expected = 1;
+common.expected = 2;
 
 var template = fs.readFileSync('test/templates/blog.html', 'utf8');
 var context = require('./contexts/blog').context;
@@ -13,8 +15,13 @@ var partials = {
 };
 
 var rendered = selleck.render(template, context, partials);
+var compiled = selleck.compile(template, partials);
+
 // uncomment to update expected
 //fs.writeFileSync('test/compiled/blog.js', compiled);
 //fs.writeFileSync('test/rendered/blog.html', rendered);
+
 var renderedExpected = fs.readFileSync('test/rendered/blog.html', 'utf8');
 assert.equal(rendered, renderedExpected, 'rendered and expected differ');
+var compiledExpected = fs.readFileSync('test/compiled/blog.js', 'utf8');
+assert.equal(compiled, compiledExpected, 'compiled and expected differ');
