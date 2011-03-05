4e049427823e1b1f9dddc60d45aaf135ce58e501
Gabriel Farrell <gsf747@gmail.com>
2011-03-05 18:33:26 -0500

    Added test-escape

---
+++ test/test-escape.js
@@ -0,0 +1,18 @@
+// test escaping of tags
+
+var fs = require('fs');
+var common = require('./common');
+var assert = common.assert;
+var selleck = require('../lib/selleck');
+var render = selleck.render;
+
+common.expected = 8;
+
+assert.equal(render('\\{bob}'), '{bob}');
+assert.equal(render('\\{bob.bloss}'), '{bob.bloss}');
+assert.equal(render('\\{>reindeer}'), '{>reindeer}');
+assert.equal(render('\\{for anger in mgmt}'), '{for anger in mgmt}');
+assert.equal(render('\\{/for}'), '{/for}');
+assert.equal(render('\\{if then}'), '{if then}');
+assert.equal(render('\\{if not now}'), '{if not now}');
+assert.equal(render('\\{/if}'), '{/if}');
