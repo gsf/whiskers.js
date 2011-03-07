2b6712c38b94bf863146c36be67281ff2ef115f2
Gabriel Farrell <gsf747@gmail.com>
2011-03-06 22:55:48 -0500

    Convert test context to json

--- test/test-blog.js
+++ test/test-blog.js
@@ -8,7 +8,7 @@ var selleck = require('../lib/selleck');
 common.expected = 1;
 
 var template = fs.readFileSync('test/templates/blog.html', 'utf8');
-var context = require('./contexts/blog').context;
+var context = JSON.parse(fs.readFileSync('test/contexts/blog.json', 'utf8'));
 var partials = {
   comment: fs.readFileSync('test/templates/comment.html', 'utf8'),
   addcomment: fs.readFileSync('test/templates/addcomment.html', 'utf8')
