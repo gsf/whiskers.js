fb8665d8fa1aacf43c241e5654a768e5aef16872
Gabriel Farrell <gsf747@gmail.com>
2011-03-06 23:00:09 -0500

    Fixed up addcomment template

--- test/test-blog.js
+++ test/test-blog.js
@@ -17,7 +17,7 @@ var partials = {
 var rendered = selleck.render(template, context, partials);
 
 // uncomment to update expected
-fs.writeFileSync('test/rendered/blog.html', rendered);
+//fs.writeFileSync('test/rendered/blog.html', rendered);
 
 var renderedExpected = fs.readFileSync('test/rendered/blog.html', 'utf8');
 assert.equal(rendered, renderedExpected, 'rendered and expected differ');
