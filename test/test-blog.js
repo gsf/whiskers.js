f6e23d2f99f20673dcf3a64cac6fd20aa8a90adb
Gabriel Farrell <gsf747@gmail.com>
2011-03-06 22:39:50 -0500

    Render is the only exposed method

--- test/test-blog.js
+++ test/test-blog.js
@@ -5,7 +5,7 @@ var common = require('./common');
 var assert = common.assert;
 var selleck = require('../lib/selleck');
 
-common.expected = 2;
+common.expected = 1;
 
 var template = fs.readFileSync('test/templates/blog.html', 'utf8');
 var context = require('./contexts/blog').context;
@@ -15,13 +15,9 @@ var partials = {
 };
 
 var rendered = selleck.render(template, context, partials);
-var compiled = selleck.compile(template, partials);
 
 // uncomment to update expected
-//fs.writeFileSync('test/compiled/blog.js', compiled);
-//fs.writeFileSync('test/rendered/blog.html', rendered);
+fs.writeFileSync('test/rendered/blog.html', rendered);
 
 var renderedExpected = fs.readFileSync('test/rendered/blog.html', 'utf8');
 assert.equal(rendered, renderedExpected, 'rendered and expected differ');
-var compiledExpected = fs.readFileSync('test/compiled/blog.js', 'utf8');
-assert.equal(compiled, compiledExpected, 'compiled and expected differ');
