07683e31d9f3c6dd8534fb38b89b260568ae1898
Gabriel Farrell <gsf747@gmail.com>
2011-03-24 23:42:06 -0400

    Add test for function with context param

--- test/test-function.js
+++ test/test-function.js
@@ -4,12 +4,16 @@ var common = require('./common');
 var assert = common.assert;
 var render = common.whiskers.render;
 
-common.expected = 1;
+common.expected = 2;
 
 var context = {
   add2and2: function() {
     return 2+2;
+  },
+  another2: function(c) {
+    return c.add2and2()+2;
   }
 };
 
 assert.equal(render('{add2and2}', context), '4');
+assert.equal(render('{another2}', context), '6');
