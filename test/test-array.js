83ba60d023563c67418b39f7c804c0b13f324c66
Gabriel Farrell <gsf747@gmail.com>
2011-03-24 23:37:10 -0400

    More array tests

--- test/test-array.js
+++ test/test-array.js
@@ -4,9 +4,11 @@ var common = require('./common');
 var assert = common.assert;
 var whiskers = common.whiskers;
 
-common.expected = 5;
+common.expected = 11;
 
 var template = '{for x in arr}{x}{/for}';
+
+assert.equal(whiskers.render(template, {}), '')
 
 var context = {arr:[1,2,3]};
 assert.equal(whiskers.render(template, context), '123')
@@ -22,3 +24,19 @@ assert.equal(whiskers.render(template, context), '')
 
 context = {arr:function(){return [1,2,3]}};
 assert.equal(whiskers.render(template, context), '123')
+
+template = '{for x in arr}{x.y}{/for}';
+
+assert.equal(whiskers.render(template, {}), '')
+
+context = {arr:[{y:1},{y:2},{y:3}]};
+assert.equal(whiskers.render(template, context), '123')
+
+context = {arr:[1,2,3]};
+assert.equal(whiskers.render(template, context), '')
+
+context = {arr:'string'};
+assert.equal(whiskers.render(template, context), '')
+
+context = {arr:{b:'orange'}};
+assert.equal(whiskers.render(template, context), '')
