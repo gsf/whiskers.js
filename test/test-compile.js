d3cf1e1c542d868abf5eff066544166a3d901538
Gabriel Farrell <gsf747@gmail.com>
2011-03-25 01:58:40 -0400

    Moved compile tests to own file

---
+++ test/test-compile.js
@@ -0,0 +1,21 @@
+// test compiling
+
+var common = require('./common');
+var assert = common.assert;
+var whiskers = common.whiskers;
+
+common.expected = 5;
+
+assert.ok(whiskers.compile());
+assert.ok(whiskers.compile('3'));
+assert.ok(whiskers.compile(3));
+assert.ok(whiskers.compile({p:3}));
+
+var template = '{sue} and {sam} and {for x in nums}{x}{/for}';
+var context = {
+  sue: 'bob',
+  sam: 'sal',
+  nums: [1,2,3]
+}
+
+assert.equal(eval(whiskers.compile(template))(context), 'bob and sal and 123');
--- test/test-templates.js
+++ test/test-templates.js
@@ -4,7 +4,7 @@ var common = require('./common');
 var assert = common.assert;
 var whiskers = common.whiskers;
 
-common.expected = 10;
+common.expected = 6;
 
 assert.equal(whiskers.render(), '');
 assert.equal(whiskers.render('3'), '3');
@@ -12,8 +12,3 @@ assert.equal(whiskers.render(3), '3');
 assert.equal(whiskers.render([1,2,3]), '1,2,3');
 assert.equal(whiskers.render({p:3}), '[object Object]');
 assert.equal(whiskers.render(function(){return 3}), 'function (){return 3}');
-
-assert.ok(whiskers.compile());
-assert.ok(whiskers.compile('3'));
-assert.ok(whiskers.compile(3));
-assert.ok(whiskers.compile({p:3}));
