72015c585bb22b79ee108e1bc28e1a679416a64f
Gabriel Farrell <gsf747@gmail.com>
2011-04-17 23:27:33 -0400

    Removed test for function passing in context

--- test/test-function.js
+++ test/test-function.js
@@ -4,16 +4,12 @@ var common = require('./common');
 var assert = common.assert;
 var render = common.whiskers.render;
 
-common.expected = 2;
+common.expected = 1;
 
 var context = {
   add2and2: function() {
     return 2+2;
-  },
-  another2: function(c) {
-    return c.add2and2()+2;
   }
 };
 
 assert.equal(render('{add2and2}', context), '4');
-assert.equal(render('{another2}', context), '6');
