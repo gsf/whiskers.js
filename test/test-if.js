83c50983ce18db4a62505c0278ac373b2b9baa4e
Gabriel Farrell <gsf747@gmail.com>
2011-04-25 15:22:12 -0400

    Trying out an "else" tag
    Still not correct behavior in complex conditions

--- test/test-if.js
+++ test/test-if.js
@@ -2,12 +2,37 @@
 
 var common = require('./common');
 var assert = common.assert;
-var whiskers = common.whiskers;
+var render = common.whiskers.render;
 
-common.expected = 4;
+common.expected = 11;
 
 var context = {foo:'bar'};
-assert.equal(whiskers.render('{if foo}{foo}{/if}', context), 'bar');
-assert.equal(whiskers.render('{if biz}{foo}{/if}', context), '');
-assert.equal(whiskers.render('{if not foo}{foo}{/if}', context), '');
-assert.equal(whiskers.render('{if not biz}{foo}{/if}', context), 'bar');
+assert.equal(render('{if foo}{foo}{/if}', context), 'bar');
+assert.equal(render('{if biz}{foo}{/if}', context), '');
+assert.equal(render('{if not foo}{foo}{/if}', context), '');
+assert.equal(render('{if not biz}{foo}{/if}', context), 'bar');
+assert.equal(render('{if foo}{foo}{else}bim{/if}', context), 'bar');
+assert.equal(render('{if biz}{foo}{else}bim{/if}', context), 'bim');
+
+// stub out console.warn
+var temp = console.warn;
+var warnings = [];
+console.warn = function(message) {
+  warnings.push(message);
+};
+
+assert.equal(render('{if biz}{foo}{else}bim{else}bam{/if}', context), 'bimbam');
+
+var template = '{else}{if biz}{foo}{else}bim{else}bam{/if}{else}{if fob}{else}';
+assert.equal(render(template, context), 'bimbam');
+
+var template = '{else}{if biz}{foo}{else}bim{if foo}bam{else}bom{else}';
+assert.equal(render(template, context), 'bimbam');
+
+var template = '{if foo}{foo}{if biz}{foo}{else}bim{/if}{else}bom{else}';
+assert.equal(render(template, context), 'barbim');
+
+assert.equal(warnings.length, 12);
+
+// return to normal
+console.warn = temp;
