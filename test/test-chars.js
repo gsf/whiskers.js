08684b7085fac3f333f409c76f7706cbf7507aee
Gabriel Farrell <g@grrawr.com>
2011-06-13 00:43:31 -0400

    Look out for carriage returns

--- test/test-chars.js
+++ test/test-chars.js
@@ -4,9 +4,11 @@ var common = require('./common');
 var assert = common.assert;
 var whiskers = common.whiskers;
 
-common.expected = 4;
+common.expected = 6;
 
 assert.equal(whiskers.render('\\'), '\\');
 assert.equal(whiskers.render('\''), '\'');
 assert.equal(whiskers.render('\\\''), '\\\'');
 assert.equal(whiskers.render('\\\'{vehicle}', {vehicle:'truck'}), '\\\'truck');
+assert.equal(whiskers.render('bob\nsue'), 'bob\nsue');
+assert.equal(whiskers.render('bob\r\nsue'), 'bob\nsue');
