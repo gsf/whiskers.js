959524cb65f91f171916fea66c6a04f6ad5f2156
Gabriel Farrell <g@grrawr.com>
2012-01-15 13:41:34 -0500

    Tests now in sync with zero falsiness

--- test/browser/test-falsy.js
+++ test/browser/test-falsy.js
@@ -1,19 +1,28 @@
 // test for falsy values
 
-test('falsy', 8, function() {
+test('falsy', 15, function() {
   var context = {
-    zero: 0,
     'false': false,
-    empty: ''
+    empty: '',
+    'null': null,
+    zero: 0
   };
 
-  equal(whiskers.render('{zero}', context), '0');
-  equal(whiskers.render('{false}', context), 'false');
+  equal(whiskers.render('{false}', context), '');
   equal(whiskers.render('{empty}', context), '');
+  equal(whiskers.render('{null}', context), '');
   equal(whiskers.render('{undefined}', context), '');
+  equal(whiskers.render('{zero}', context), '');
 
-  equal(whiskers.render('{if zero}{zero}{/if}', context), '');
-  equal(whiskers.render('{if false}{false}{/if}', context), '');
-  equal(whiskers.render('{if not zero}{zero}{/if}', context), '0');
-  equal(whiskers.render('{if not false}{false}{/if}', context), 'false');
+  equal(whiskers.render('{if false}x{/if}', context), '');
+  equal(whiskers.render('{if empty}x{/if}', context), '');
+  equal(whiskers.render('{if null}x{/if}', context), '');
+  equal(whiskers.render('{if undefined}x{/if}', context), '');
+  equal(whiskers.render('{if zero}x{/if}', context), '');
+
+  equal(whiskers.render('{if not false}x{/if}', context), 'x');
+  equal(whiskers.render('{if not empty}x{/if}', context), 'x');
+  equal(whiskers.render('{if not null}x{/if}', context), 'x');
+  equal(whiskers.render('{if not undefined}x{/if}', context), 'x');
+  equal(whiskers.render('{if not zero}x{/if}', context), 'x');
 });
--- test/server/test-falsy.js
+++ test/server/test-falsy.js
@@ -4,20 +4,29 @@ var common = require('./common');
 var assert = common.assert;
 var whiskers = common.whiskers;
 
-common.expected = 8;
+common.expected = 15;
 
 var context = {
-  zero: 0,
   'false': false,
-  empty: ''
+  empty: '',
+  'null': null,
+  zero: 0
 };
 
-assert.equal(whiskers.render('{zero}', context), '0');
-assert.equal(whiskers.render('{false}', context), 'false');
+assert.equal(whiskers.render('{false}', context), '');
 assert.equal(whiskers.render('{empty}', context), '');
+assert.equal(whiskers.render('{null}', context), '');
 assert.equal(whiskers.render('{undefined}', context), '');
+assert.equal(whiskers.render('{zero}', context), '');
 
-assert.equal(whiskers.render('{if zero}{zero}{/if}', context), '');
-assert.equal(whiskers.render('{if false}{false}{/if}', context), '');
-assert.equal(whiskers.render('{if not zero}{zero}{/if}', context), '0');
-assert.equal(whiskers.render('{if not false}{false}{/if}', context), 'false');
+assert.equal(whiskers.render('{if false}x{/if}', context), '');
+assert.equal(whiskers.render('{if empty}x{/if}', context), '');
+assert.equal(whiskers.render('{if null}x{/if}', context), '');
+assert.equal(whiskers.render('{if undefined}x{/if}', context), '');
+assert.equal(whiskers.render('{if zero}x{/if}', context), '');
+
+assert.equal(whiskers.render('{if not false}x{/if}', context), 'x');
+assert.equal(whiskers.render('{if not empty}x{/if}', context), 'x');
+assert.equal(whiskers.render('{if not null}x{/if}', context), 'x');
+assert.equal(whiskers.render('{if not undefined}x{/if}', context), 'x');
+assert.equal(whiskers.render('{if not zero}x{/if}', context), 'x');
