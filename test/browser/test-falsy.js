ecd68d2411988f3a0ed467d6c3b56a1805671e6c
Gabriel Farrell <g@grrawr.com>
2012-05-12 13:33:11 -0400

    Added deep undefined get tests

--- test/browser/test-falsy.js
+++ test/browser/test-falsy.js
@@ -1,6 +1,6 @@
 // test for falsy values
 
-test('falsy', 15, function() {
+test('falsy', 18, function() {
   var context = {
     'false': false,
     empty: '',
@@ -12,17 +12,20 @@ test('falsy', 15, function() {
   equal(whiskers.render('{empty}', context), '');
   equal(whiskers.render('{null}', context), '');
   equal(whiskers.render('{undefined}', context), '');
+  equal(whiskers.render('{undefined.undefined}', context), '');
   equal(whiskers.render('{zero}', context), '');
 
   equal(whiskers.render('{if false}x{/if}', context), '');
   equal(whiskers.render('{if empty}x{/if}', context), '');
   equal(whiskers.render('{if null}x{/if}', context), '');
   equal(whiskers.render('{if undefined}x{/if}', context), '');
+  equal(whiskers.render('{if undefined.undefined}x{/if}', context), '');
   equal(whiskers.render('{if zero}x{/if}', context), '');
 
   equal(whiskers.render('{if not false}x{/if}', context), 'x');
   equal(whiskers.render('{if not empty}x{/if}', context), 'x');
   equal(whiskers.render('{if not null}x{/if}', context), 'x');
   equal(whiskers.render('{if not undefined}x{/if}', context), 'x');
+  equal(whiskers.render('{if not undefined.undefined}x{/if}', context), 'x');
   equal(whiskers.render('{if not zero}x{/if}', context), 'x');
 });
