93cee6cc96bdce9964d4379b57c41739583d1250
Gabriel Farrell <g@grrawr.com>
2013-01-23 01:47:31 -0500

    Tests for empty object and accessing 0 in array

--- test/server/test-falsy.js
+++ test/server/test-falsy.js
@@ -4,14 +4,16 @@ var common = require('./common');
 var assert = common.assert;
 var whiskers = common.whiskers;
 
-common.expected = 21;
+common.expected = 23;
 
 var context = {
   'false': false,
   empty: '',
   'null': null,
   zero: 0,
-  emptyArray: []
+  emptyArray: [],
+  emptyObj: {},
+  zeroArray: [{value: 0}]
 };
 
 assert.equal(whiskers.render('{false}', context), '');
@@ -21,6 +23,8 @@ assert.equal(whiskers.render('{undefined}', context), '');
 assert.equal(whiskers.render('{undefined.undefined}', context), '');
 assert.equal(whiskers.render('{zero}', context), '0');
 assert.equal(whiskers.render('{emptyArray}', context), '');
+assert.equal(whiskers.render('{emptyObj}', context), '');
+assert.equal(whiskers.render('{zeroArray.0.value}', context), '0');
 
 assert.equal(whiskers.render('{if false}x{/if}', context), '');
 assert.equal(whiskers.render('{if empty}x{/if}', context), '');
@@ -28,7 +32,6 @@ assert.equal(whiskers.render('{if null}x{/if}', context), '');
 assert.equal(whiskers.render('{if undefined}x{/if}', context), '');
 assert.equal(whiskers.render('{if undefined.undefined}x{/if}', context), '');
 assert.equal(whiskers.render('{if zero}x{/if}', context), '');
-assert.equal(whiskers.render('{for x in emptyArray}x{/for}', context), '');
 
 assert.equal(whiskers.render('{if not false}x{/if}', context), 'x');
 assert.equal(whiskers.render('{if not empty}x{/if}', context), 'x');
@@ -36,4 +39,6 @@ assert.equal(whiskers.render('{if not null}x{/if}', context), 'x');
 assert.equal(whiskers.render('{if not undefined}x{/if}', context), 'x');
 assert.equal(whiskers.render('{if not undefined.undefined}x{/if}', context), 'x');
 assert.equal(whiskers.render('{if not zero}x{/if}', context), 'x');
+
 assert.equal(whiskers.render('{for x in emptyArray}blah{else}x{/for}', context), 'x');
+assert.equal(whiskers.render('{for x in zeroArray}{x.value}{else}blah{/for}', context), '0');
