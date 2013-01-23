3233ef7a19266704024b540a513ee2e42f5fb45e
Gabriel Farrell <g@grrawr.com>
2013-01-22 22:19:13 -0500

    Modified tests for this new era of falsiness

--- test/server/test-falsy.js
+++ test/server/test-falsy.js
@@ -4,13 +4,14 @@ var common = require('./common');
 var assert = common.assert;
 var whiskers = common.whiskers;
 
-common.expected = 18;
+common.expected = 21;
 
 var context = {
   'false': false,
   empty: '',
   'null': null,
-  zero: 0
+  zero: 0,
+  emptyArray: []
 };
 
 assert.equal(whiskers.render('{false}', context), '');
@@ -18,7 +19,8 @@ assert.equal(whiskers.render('{empty}', context), '');
 assert.equal(whiskers.render('{null}', context), '');
 assert.equal(whiskers.render('{undefined}', context), '');
 assert.equal(whiskers.render('{undefined.undefined}', context), '');
-assert.equal(whiskers.render('{zero}', context), '');
+assert.equal(whiskers.render('{zero}', context), '0');
+assert.equal(whiskers.render('{emptyArray}', context), '');
 
 assert.equal(whiskers.render('{if false}x{/if}', context), '');
 assert.equal(whiskers.render('{if empty}x{/if}', context), '');
@@ -26,6 +28,7 @@ assert.equal(whiskers.render('{if null}x{/if}', context), '');
 assert.equal(whiskers.render('{if undefined}x{/if}', context), '');
 assert.equal(whiskers.render('{if undefined.undefined}x{/if}', context), '');
 assert.equal(whiskers.render('{if zero}x{/if}', context), '');
+assert.equal(whiskers.render('{for x in emptyArray}x{/for}', context), '');
 
 assert.equal(whiskers.render('{if not false}x{/if}', context), 'x');
 assert.equal(whiskers.render('{if not empty}x{/if}', context), 'x');
@@ -33,3 +36,4 @@ assert.equal(whiskers.render('{if not null}x{/if}', context), 'x');
 assert.equal(whiskers.render('{if not undefined}x{/if}', context), 'x');
 assert.equal(whiskers.render('{if not undefined.undefined}x{/if}', context), 'x');
 assert.equal(whiskers.render('{if not zero}x{/if}', context), 'x');
+assert.equal(whiskers.render('{for x in emptyArray}blah{else}x{/for}', context), 'x');
